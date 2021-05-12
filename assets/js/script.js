478958183fe8250c1d1957a034596c16a9f57df8
Ethan Harsh <ethan@ethanharsh.com>
2021-05-12 19:17:58 +0000

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,19 +13,21 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+//Generate password string
 function generatePassword () {
+  //Create arrays to draw characters from
   const numArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
   const lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   const upperArray = ['A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   const symbolArray = ['@', '!', '$', '&'];
-  
+  //Create array of prompt question strings
   const questionArray = ['First, would you like to have lowercase letters in your password?', 'Thank you! \n Would you like to have UPPERCASE letters in your password?', 'Awesome 😎 \n Would you like to have numbers in your password?', 'Last question! \n Would you like to have symbols in your password? (@, !, $, &)'];
+  //Create array of other arrays
   const bigArray = [lowerArray, upperArray, numArray, symbolArray];
-
+  //initialize password variables
   let passArray = [];
   let pass = '';
-
+  //recieve password string length from user
   var check = false;
   while (check === false){
     var len = parseInt(prompt('Please enter password length. (Between 8 - 128 characters)'));
@@ -34,7 +36,7 @@ function generatePassword () {
     }
   }
   //console.log(len);
-  
+  //Ask each password preference and concat choices to main password array
   let i = 0;
   questionArray.forEach(el => {
     if(charPromp(el)) {
@@ -42,12 +44,12 @@ function generatePassword () {
     }
     i++;
   });
-
+  //randomly select characters from character preference array then add to password string
   for(i=0; i < len; i++) {
     pass = pass.concat(passArray[Math.floor(Math.random() * passArray.length)]);
     //console.log('Password: ' + pass);
   }
-
+  //return password string
   return pass;
   //console.log(passArray);
 }
